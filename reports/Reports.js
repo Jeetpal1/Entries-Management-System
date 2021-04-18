b1577077db941d0d8a4857fad8d019096c9805f2
Jeetpal <singhjeetpal99@gmail.com>
2021-04-18 09:44:24 -0500

    Some changes in constructor (reports.js), loadProjects(), fillProjectsWithResponse(), handleProjectChange(event), loadUsers() filllUsersWithResponse(), fillTimeEntriesWithResponse, and changed dateString(date) to accept Date object instead of String

--- reports/Reports.js
+++ reports/Reports.js
@@ -8,15 +8,16 @@ class Reports {
 		// Must filled via the API calls
 		this.projects = undefined;
 		this.users = undefined;
+	
 
 		this.api = api;
 		this.company_id = company_id;
 
 		// INSERT YOUR CODE BELOW THIS LINE
-		if(this.projects!=undefined&&this.users!=undefined)
-		{
-			loadProjects();
-		}
+		this.loadProjects();
+		this.loadUsers();
+		
+		document.getElementById('project_id').addEventListener('change',this.handleProjectChange.bind(this));
 	}
 
 	/////////////////////////////////////////////
@@ -30,25 +31,37 @@ class Reports {
 	{
 		console.log('----- loadProjects -----');
 		// INSERT YOUR CODE BELOW THIS LINE
-		api.makeRequest('GET','acs/profile', {}, this.fillProjectsWithResponse);
+		let pathForProjects = `companies/${this.company_id}/projects`;
+		api.makeRequest('GET', pathForProjects, {}, this.fillProjectsWithResponse.bind(this));		
 	}
 
 	fillProjectsWithResponse(xhr_response)
 	{
 		console.log('----- fillProjectsWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
-		let tableBody = document.getElementById("results");
-		tableBody.insertAdjacentText('afterbegin','sdfjlkdsajvsdiovjawlekngflkwenr');
+		let projectList = document.getElementById('project_id');
+		for(let project in xhr_response){
+			projectList.insertAdjacentHTML('beforeend',`<option value="${xhr_response[project].project_id}">${xhr_response[project].title}</option>`);
+			
+		}
+		this.projects = xhr_response;
 		
-		/*for(let project in xhr_response){
-
-		}*/
 	}
 
 	handleProjectChange(event)
 	{
 		console.log('----- handleProjectChange -----', event);
 		// INSERT YOUR CODE BELOW THIS LINE
+		
+		
+		let tbody = document.querySelector('#results tbody').rows;
+		console.log(tbody)
+		for (let row in tbody){
+			if (tbody[row].id != event.currentTarget.options[event.currentTarget.selectedIndex].value){
+				document.getElementById(`#${tbody[row].id}`).toggleId;  //you were here
+			}
+		}
+		
 	}
 
 
@@ -62,12 +75,23 @@ class Reports {
 	{
 		console.log('----- loadUsers -----');
 		// INSERT YOUR CODE BELOW THIS LINE
+		let pathForUsers = `companies/${this.company_id}/users`;
+		api.makeRequest('GET', pathForUsers, {}, this.fillUsersWithResponse.bind(this));
+		setTimeout(() => {if(this.users!=undefined && this.projects!=undefined){
+			this.loadTimeEntries();
+		}},2000);
 	}
 
 	fillUsersWithResponse(xhr_response)
 	{
 		console.log('----- fillUsersWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
+		let userList = document.getElementById('user_id');
+		for(let user in xhr_response){
+			userList.insertAdjacentHTML('beforeend',`<option value="${xhr_response[user].user_id}">${xhr_response[user].first_name} ${xhr_response[user].last_name}</option>`);
+		}
+		this.users = xhr_response;
+		
 	}
 
 	handleUserChange(event)
@@ -86,23 +110,42 @@ class Reports {
 	{
 		console.log('----- loadTimeEntries -----');
 		// INSERT YOUR CODE BELOW THIS LINE
+	
+		
 	}
 
 	fillTimeEntriesWithResponse(xhr_response)
 	{
 		console.log('----- fillTimeEntriesWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
+		let tableBody = document.querySelector('#results tbody');
+
+		// to go through every entries in the entry list and to insert required data in table cells
+		for(let entry in xhr_response){
+
+			//to get total number of seconds in between the start and end date, I am using Date objects
+			let startDate = new Date(xhr_response[entry].start_time);
+			let endDate = new Date(xhr_response[entry].end_time);
+			let totalSeconds = (endDate.getTime()-startDate.getTime())/1000;
+
+			// to get total number of seconds in hours:munites:seconds format, I am using convertSecondsToHoursMinutesSeconds() that is in config.js
+			let formattedTime = convertSecondsToHoursMinutesSeconds(totalSeconds);
+			tableBody.insertAdjacentHTML('afterbegin',`
+			<tr id = "${xhr_response[entry].project_id}">
+				<td>${xhr_response[entry].description}</td>
+				<td>${this.projects[xhr_response[entry].project_id].title}</td>
+				<td>${this.users[xhr_response[entry].user_id].first_name} ${this.users[xhr_response[entry].user_id].last_name}</td>
+				<td>${formattedTime}</td>
+				<td>${this.dateString(startDate)}</td>
+			</tr>
+			`);
+		}
 	}
 
 	dateString(date)
 	{
 		var months = ['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		var year = date.substr(0,4);
-		var month = date.substr(5,2);
-		var day = date.substr(8,2);
 
-		return months[parseInt(month)] + ' ' + day + ', ' + year;
-
+		return months[date.getMonth()+1] + ' ' + date.getDate() + ', ' + date.getFullYear();
 	}
-
 }
