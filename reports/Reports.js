34a6e965544200aabc4c59972d388788040b42cc
Jeetpal <singhjeetpal99@gmail.com>
2021-04-19 16:09:59 -0500

    Wrote handleUserChange and handleProjectChange which performs the operation to filter time entries
    Added EventListerners in constructor using "Event delegation"
    Updated fillTimeEntriesWithResponse to add dataset attribute to table rows

--- reports/Reports.js
+++ reports/Reports.js
@@ -3,12 +3,11 @@
  */
 class Reports {
 
-	constructor(api, company_id)
-	{
+	constructor(api, company_id) {
 		// Must filled via the API calls
 		this.projects = undefined;
 		this.users = undefined;
-	
+
 
 		this.api = api;
 		this.company_id = company_id;
@@ -16,8 +15,16 @@ class Reports {
 		// INSERT YOUR CODE BELOW THIS LINE
 		this.loadProjects();
 		this.loadUsers();
-		
-		document.getElementById('project_id').addEventListener('change',this.handleProjectChange.bind(this));
+
+		document.getElementById('reports_filter_form').addEventListener('change', (event) => {
+			if (event.target.id == 'project_id') {
+				this.handleProjectChange(event);
+			}
+			if (event.target.id == 'user_id') {
+				this.handleUserChange(event);
+			}
+		});
+
 	}
 
 	/////////////////////////////////////////////
@@ -27,41 +34,73 @@ class Reports {
 	/////////////////////////////////////////////
 
 
-	loadProjects()
-	{
+	loadProjects() {
 		console.log('----- loadProjects -----');
 		// INSERT YOUR CODE BELOW THIS LINE
 		let pathForProjects = `companies/${this.company_id}/projects`;
-		api.makeRequest('GET', pathForProjects, {}, this.fillProjectsWithResponse.bind(this));		
+		api.makeRequest('GET', pathForProjects, {}, this.fillProjectsWithResponse.bind(this));
 	}
 
-	fillProjectsWithResponse(xhr_response)
-	{
+	fillProjectsWithResponse(xhr_response) {
 		console.log('----- fillProjectsWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
 		let projectList = document.getElementById('project_id');
-		for(let project in xhr_response){
-			projectList.insertAdjacentHTML('beforeend',`<option value="${xhr_response[project].project_id}">${xhr_response[project].title}</option>`);
-			
+		for (let project in xhr_response) {
+			projectList.insertAdjacentHTML('beforeend', `<option value="${xhr_response[project].project_id}">${xhr_response[project].title}</option>`);
+
 		}
 		this.projects = xhr_response;
-		
+
 	}
 
-	handleProjectChange(event)
-	{
+	handleProjectChange(event) {
 		console.log('----- handleProjectChange -----', event);
 		// INSERT YOUR CODE BELOW THIS LINE
-		
-		
+
+		let valueOfProjectOption = event.target.value;
 		let tbody = document.querySelector('#results tbody').rows;
-		console.log(tbody)
-		for (let row in tbody){
-			if (tbody[row].id != event.currentTarget.options[event.currentTarget.selectedIndex].value){
-				document.getElementById(`#${tbody[row].id}`).toggleId;  //you were here
+		let userSelectForm = document.getElementById('user_id');
+
+		if (valueOfProjectOption == 'default') {
+			for (let row in tbody) {
+				if (tbody[row].tagName == 'TR') {
+					if (userSelectForm.value != 'default') {
+						if (tbody[row].dataset.userId == userSelectForm.value) {
+							tbody[row].hidden = false;
+						}
+						else {
+							tbody[row].hidden = true;
+						}
+
+					}
+					else {
+						tbody[row].hidden = false;
+					}
+				}
 			}
 		}
-		
+		else {
+			for (let row in tbody) {
+				if (userSelectForm.value == 'default' && tbody[row].tagName == 'TR') {
+					if (valueOfProjectOption == tbody[row].dataset.projectId) {
+						tbody[row].hidden = false;
+					}
+					else {
+						tbody[row].hidden = true;
+					}
+				}
+				else {
+					if (tbody[row].tagName == 'TR') {
+						if (valueOfProjectOption == tbody[row].dataset.projectId && userSelectForm.value == tbody[row].dataset.userId) {
+							tbody[row].hidden = false;
+						}
+						else {
+							tbody[row].hidden = true;
+						}
+					}
+				}
+			}
+		}
 	}
 
 
@@ -71,33 +110,77 @@ class Reports {
 	//
 	/////////////////////////////////////////////
 
-	loadUsers()
-	{
+	loadUsers() {
 		console.log('----- loadUsers -----');
 		// INSERT YOUR CODE BELOW THIS LINE
 		let pathForUsers = `companies/${this.company_id}/users`;
 		api.makeRequest('GET', pathForUsers, {}, this.fillUsersWithResponse.bind(this));
-		setTimeout(() => {if(this.users!=undefined && this.projects!=undefined){
-			this.loadTimeEntries();
-		}},2000);
+		setTimeout(() => {
+			if (this.users != undefined && this.projects != undefined) {
+				this.loadTimeEntries();
+			}
+		}, 2000);
 	}
 
-	fillUsersWithResponse(xhr_response)
-	{
+	fillUsersWithResponse(xhr_response) {
 		console.log('----- fillUsersWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
 		let userList = document.getElementById('user_id');
-		for(let user in xhr_response){
-			userList.insertAdjacentHTML('beforeend',`<option value="${xhr_response[user].user_id}">${xhr_response[user].first_name} ${xhr_response[user].last_name}</option>`);
+		for (let user in xhr_response) {
+			userList.insertAdjacentHTML('beforeend', `<option value="${xhr_response[user].user_id}">${xhr_response[user].first_name} ${xhr_response[user].last_name}</option>`);
 		}
 		this.users = xhr_response;
-		
+
 	}
 
-	handleUserChange(event)
-	{
+	handleUserChange(event) {
 		console.log('----- handleUserChange -----', event);
 		// INSERT YOUR CODE BELOW THIS LINE
+
+		let valueOfUserOption = event.target.value;
+		let tbody = document.querySelector('#results tbody').rows;
+		let projectSelectForm = document.getElementById('project_id');
+
+		if (valueOfUserOption == 'default') {
+			for (let row in tbody) {
+				if (tbody[row].tagName == 'TR') {
+					if (projectSelectForm.value != 'default') {
+						if (tbody[row].dataset.projectId == projectSelectForm.value) {
+							tbody[row].hidden = false;
+						}
+						else {
+							tbody[row].hidden = true;
+						}
+
+					}
+					else {
+						tbody[row].hidden = false;
+					}
+				}
+			}
+		}
+		else {
+			for (let row in tbody) {
+				if (projectSelectForm.value == 'default' && tbody[row].tagName == 'TR') {
+					if (valueOfUserOption == tbody[row].dataset.userId) {
+						tbody[row].hidden = false;
+					}
+					else {
+						tbody[row].hidden = true;
+					}
+				}
+				else {
+					if (tbody[row].tagName == 'TR') {
+						if (valueOfUserOption == tbody[row].dataset.userId && projectSelectForm.value == tbody[row].dataset.projectId) {
+							tbody[row].hidden = false;
+						}
+						else {
+							tbody[row].hidden = true;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	/////////////////////////////////////////////
@@ -106,35 +189,33 @@ class Reports {
 	//
 	/////////////////////////////////////////////
 
-	loadTimeEntries()
-	{
+	loadTimeEntries() {
 		console.log('----- loadTimeEntries -----');
 		// INSERT YOUR CODE BELOW THIS LINE
-		for(let project in this.projects){
+		for (let project in this.projects) {
 			let pathForEntries = `projects/${this.projects[project].project_id}/entries`;
 			api.makeRequest('GET', pathForEntries, {}, this.fillTimeEntriesWithResponse.bind(this));
 		}
-		
+
 	}
 
-	fillTimeEntriesWithResponse(xhr_response)
-	{
+	fillTimeEntriesWithResponse(xhr_response) {
 		console.log('----- fillTimeEntriesWithResponse -----', xhr_response);
 		// INSERT YOUR CODE BELOW THIS LINE
 		let tableBody = document.querySelector('#results tbody');
 
 		// to go through every entries in the entry list and to insert required data in table cells
-		for(let entry in xhr_response){
+		for (let entry in xhr_response) {
 
 			//to get total number of seconds in between the start and end date, I am using Date objects
 			let startDate = new Date(xhr_response[entry].start_time);
 			let endDate = new Date(xhr_response[entry].end_time);
-			let totalSeconds = (endDate.getTime()-startDate.getTime())/1000;
+			let totalSeconds = (endDate.getTime() - startDate.getTime()) / 1000;
 
 			// to get total number of seconds in hours:munites:seconds format, I am using convertSecondsToHoursMinutesSeconds() that is in config.js
 			let formattedTime = convertSecondsToHoursMinutesSeconds(totalSeconds);
-			tableBody.insertAdjacentHTML('afterbegin',`
-			<tr id = "${xhr_response[entry].project_id}">
+			tableBody.insertAdjacentHTML('afterbegin', `
+			<tr data-project-id="${xhr_response[entry].project_id}" data-user-id="${xhr_response[entry].user_id}">
 				<td>${xhr_response[entry].description}</td>
 				<td>${this.projects[xhr_response[entry].project_id].title}</td>
 				<td>${this.users[xhr_response[entry].user_id].first_name} ${this.users[xhr_response[entry].user_id].last_name}</td>
@@ -145,10 +226,9 @@ class Reports {
 		}
 	}
 
-	dateString(date)
-	{
-		var months = ['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	dateString(date) {
+		var months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-		return months[date.getMonth()+1] + ' ' + date.getDate() + ', ' + date.getFullYear();
+		return months[date.getMonth() + 1] + ' ' + date.getDate() + ', ' + date.getFullYear();
 	}
 }
