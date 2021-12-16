b5ca0eca806fc55d90c1182a4c938eae87af84ba
Jeetpal Singh <70360391+Jeetpal1@users.noreply.github.com>
2021-12-16 10:37:52 -0600

    Update Track.js

--- track/Track.js
+++ track/Track.js
@@ -1,7 +1,7 @@
 /**
  * A class that handles anything to do with the Reports view
  */
-//Person A: Time Tracker: Saman Fathnazarian
+//Person A: Time Tracker: 
 class Track {
     /**
      * Constructor for the Track. Initialize a new Track object.
